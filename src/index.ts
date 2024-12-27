2aa541539555c8d133176b3d757b169fdedf4cf0
Svenlaa <mail@svenlaa.com>
2024-12-27 20:46:33 +0100

    capital s

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = new Hono()
     .use(cors({ origin: '*' }))
     .get('/', (c) =>
         c.html(
-            '<meta name="viewport" content="width=device-width, initial-scale=1.0" /><link rel="stylesheet" href="https://svenlaa.com/style.css" /><title>api.svenlaa.com</title><h1><a style="--color:var(--stone-950);text-decoration:none" href="https://github.com/svenlaa/api.svenlaa.com">api.svenlaa.com</a></h1>Why are you here? Visit <a href="https://svenlaa.com">svenlaa.com</a> for the site'
+            '<meta name="viewport" content="width=device-width, initial-scale=1.0" /><link rel="stylesheet" href="https://svenlaa.com/style.css" /><title>api.svenlaa.com</title><h1><a style="--color:var(--stone-950);text-decoration:none" href="https://github.com/Svenlaa/api.svenlaa.com">api.svenlaa.com</a></h1>Why are you here? Visit <a href="https://svenlaa.com">svenlaa.com</a> for the site'
         )
     );
 
